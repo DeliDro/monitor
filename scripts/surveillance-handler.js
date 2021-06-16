033139c04ab52450a97e419bd49482b0de130f97
darn40 <73474648+darn40@users.noreply.github.com>
2021-06-16 17:38:26 +0000

    16/06/2021 17:19 les logs

--- scripts/surveillance-handler.js
+++ scripts/surveillance-handler.js
@@ -261,6 +261,7 @@ function ajouterAction(lieu, id){
     let nombreDeActions = document.getElementById(id).getElementsByTagName('input').length
     let a = `
         <div class="flex items-center w-full mb-2" id="${lieu}-${nombreDeActions}">
+            <p><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path d="M42.7 469.3c0 23.5 19.1 42.7 42.7 42.7h341.3c23.5 0 42.7-19.1 42.7-42.7V192H42.7v277.3zm320-213.3h42.7v192h-42.7V256zm-128 0h42.7v192h-42.7V256zm-128 0h42.7v192h-42.7V256zm384-170.7h-128V42.7C362.7 19.1 343.5 0 320 0H192c-23.5 0-42.7 19.1-42.7 42.7v42.7h-128C9.5 85.3 0 94.9 0 106.7V128c0 11.8 9.5 21.3 21.3 21.3h469.3c11.8 0 21.3-9.5 21.3-21.3v-21.3c.1-11.8-9.4-21.4-21.2-21.4zm-170.7 0H192V42.7h128v42.6z" fill="red"/><rect x="0" y="0" width="512" height="512" fill="rgba(0, 0, 0, 0)" /></svg>&nbsp;:&nbsp;</p>
             <p>Action ${nombreDeActions+1}&nbsp;:&nbsp;</p>
             <select
                 name="${lieu}-${nombreDeActions}-1"
